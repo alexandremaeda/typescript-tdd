61e4514dcf9eccc00978b4c80a467aaedf2f97b3
Alexandre Maeda <ale.b.maeda@gmail.com>
2023-03-25 18:26:09 -0300

    Ensure LocalAddUser deletes old cache on save

--- src/domain/useCases/data/useCases/local-add-user.spec.ts
+++ src/domain/useCases/data/useCases/local-add-user.spec.ts
@@ -1,12 +1,24 @@
 class LocalAddUser {
   constructor(private readonly cacheStore: CacheStore) {}
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
+  }
 }
 
-interface CacheStore {}
+interface CacheStore {
+  delete: () => void;
+}
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
+
+// sut = system under test
 
 describe("LocalAddUser", () => {
   test("should not delete cache on sut.init", () => {
@@ -14,5 +26,14 @@ describe("LocalAddUser", () => {
     new LocalAddUser(cacheStore);
 
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test("should delete old cache on sut.save", async () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalAddUser(cacheStore);
+
+    await sut.save();
+
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
